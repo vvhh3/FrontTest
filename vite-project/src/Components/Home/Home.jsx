03531c307a6fdb0e48bfb7fb8e7063b66d9c4e98
vvhh3 <matveialex2007@gmail.com>
2026-03-27 15:20:04 +0400

    bogdan pososi

--- vite-project/src/Components/Home/Home.jsx
+++ vite-project/src/Components/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { useBearStore } from '../zustand/useBearStore';
-
+import { House, DatabaseZap, Package, HardDrive, Braces, Cookie } from 'lucide-react'
 
 const Home = () => {
 
@@ -11,27 +11,41 @@ const Home = () => {
     }
 
     return (
-        <div>
-            <div className="bg-black text-white p-4 dark:bg-white dark:text-black ">
+        <div class="font-mono">
+            <div className='bg-black text-white p-4 dark:bg-white dark:text-black'>
                 TEST
             </div>
-            <div className='bg-black w-20 m-2 dark:border-zinc-600 dark:w-20 dark:m-2  dark:bg-white'>
-                <Link to="/" className="no-underline text-white p-4 transition-colors  dark:text-black duration-300 hover:text-red-600">Home</Link>
-            </div>
-            <div className='bg-black w-20 m-2 dark:border-zinc-600 dark:w-20 dark:m-2  dark:bg-white'>
-                <Link to="/Zustand" className="no-underline text-white p-4 transition-colors duration-300  dark:text-black hover:text-red-600">Zustand</Link>
-            </div>
-            <div className='bg-black w-20 m-2 dark:border-zinc-600 dark:w-20 dark:m-2  dark:bg-white'>
-                <Link to="/RTK" className="no-underline text-white p-4 transition-colors duration-300 dark:text-black hover:text-red-600">RTK</Link>
-            </div>
-            <div className='bg-black  w-20 m-2 dark:border-zinc-600 dark:w-20 dark:m-2  dark:bg-white'>
-                <Link to="/Redis" className="no-underline text-white p-4 transition-colors  dark:text-black duration-300 hover:text-red-600">Front</Link>
-            </div>
-            <div className='bg-black w-20 m-2 dark:border-zinc-600 dark:w-20 dark:m-2  dark:bg-white'>
-                <Link to="/JWT" className="no-underline text-white p-4 transition-colors duration-300 dark:text-black hover:text-red-600">JWT</Link>
-            </div>
-            <div className='bg-black w-20 m-2 dark:border-zinc-600 dark:w-20 dark:m-2  dark:bg-white '>
-                <Link to="/Ref" className="no-underline text-white p-4 transition-colors duration-300 dark:text-black hover:text-red-600">Ref</Link>
+            <div className='text-xl max-w-3xl'>
+                <div className='bg-black  transition-colors rounded-xl m-2 p-4 dark:border-zinc-600 dark:m-2 hover:bg-red-950 dark:hover:bg-red-200  dark:bg-white'>
+                    <Link to="/" className="no-underline text-white p-4 transition-colors  dark:text-black duration-300 hover:text-red-600 flex justify-between">
+                        Home <House />
+                    </Link>
+                </div>
+                <div className='bg-black  transition-colors rounded-xl m-2 p-4 dark:border-zinc-600 dark:m-2  hover:bg-red-950 dark:hover:bg-red-200 dark:bg-white'>
+                    <Link to="/Zustand" className="no-underline text-white p-4 transition-colors duration-300  dark:text-black hover:text-red-600  flex justify-between">
+                        Zustand <DatabaseZap />
+                    </Link>
+                </div>
+                <div className='bg-black  transition-colors rounded-xl m-2 p-4 dark:border-zinc-600  dark:m-2  hover:bg-red-950 dark:hover:bg-red-200 dark:bg-white'>
+                    <Link to="/RTK" className="no-underline text-white p-4 transition-colors duration-300 dark:text-black hover:text-red-600  flex justify-between">
+                        RTK <Package />
+                    </Link>
+                </div>
+                <div className='bg-black  transition-colors rounded-xl m-2 p-4 dark:border-zinc-600   dark:m-2  hover:bg-red-950 dark:hover:bg-red-200 dark:bg-white'>
+                    <Link to="/Redis" className="no-underline text-white p-4 transition-colors  dark:text-black duration-300 hover:text-red-600  flex justify-between">
+                        Front <HardDrive />
+                    </Link>
+                </div>
+                <div className='bg-black  transition-colors rounded-xl m-2 p-4 dark:border-zinc-600 dark:m-2  hover:bg-red-950 dark:hover:bg-red-200 dark:bg-white'>
+                    <Link to="/JWT" className="no-underline text-white p-4 transition-colors duration-300 dark:text-black hover:text-red-600  flex justify-between">
+                        JWT <Braces />
+                    </Link>
+                </div>
+                <div className='bg-black  transition-colors rounded-xl m-2 p-4 dark:border-zinc-600  dark:m-2  hover:bg-red-950 dark:hover:bg-red-200 dark:bg-white '>
+                    <Link to="/Ref" className="no-underline text-white p-4 transition-colors duration-300 dark:text-black hover:text-red-600  flex justify-between">
+                        Ref <Cookie />
+                    </Link>
+                </div>
             </div>
         </div>
     );
--- vite-project/src/Components/JWT/JWT.jsx
+++ vite-project/src/Components/JWT/JWT.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { House } from 'lucide-react'
 const JWT = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
@@ -27,23 +28,46 @@ const JWT = () => {
   };
 
   return (
-    <div>
+    <div className="w-full">
       <div className="bg-black text-white p-4 dark:bg-white dark:text-black ">
         JWT
       </div>
-      <div className='bg-gray-300 w-20 m-2'>
-        <Link to="/" className="no-underline text-gray-900 p-4 transition-colors duration-300 hover:text-blue-600">Home</Link>
+      <div className="w-full">
+        <div className='bg-black  transition-colors rounded-xl m-2 p-4 dark:border-zinc-600 dark:m-2 hover:bg-red-950 dark:hover:bg-red-200  dark:bg-white'>
+          <Link to="/" className='no-underline text-white p-4 transition-colors  dark:text-black duration-300 hover:text-red-600 flex justify-between'>
+            Home <House />
+          </Link>
+        </div>
+        <section className="flex w-full items-center justify-center flex-col p-4">
+          <input placeholder="Username"
+            className="border-2 w-[50vh] border-pink-800 p-4 m-4 rounded-2xl dark:bg-white dark:text-black"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <input placeholder="Password"
+            className="border-2 w-[50vh] border-pink-800 p-4 m-4 rounded-2xl dark:bg-white dark:text-black"
+            type="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+          <div className='bg-black w-[50vh] max-w-2xl  transition-colors rounded-xl m-2 p-4 dark:border-zinc-600 dark:m-2 hover:bg-red-950 dark:hover:bg-red-200  dark:bg-white'>
+            <button onClick={register} className='no-underline text-white p-4 transition-colors  dark:text-black duration-300 hover:text-red-600 flex justify-between'>
+              Register
+            </button>
+          </div>
+          <div className='bg-black w-[50vh] max-w-2xl  transition-colors rounded-xl m-2 p-4 dark:border-zinc-600 dark:m-2 hover:bg-red-950 dark:hover:bg-red-200  dark:bg-white'>
+            <button onClick={login} className='no-underline text-white p-4 transition-colors  dark:text-black duration-300 hover:text-red-600 flex justify-between'>
+              Login
+            </button>
+          </div>
+        </section>
       </div>
-      <input placeholder="Username" value={username} onChange={e => setUsername(e.target.value)} className="border-2 border-pink-800" />
-      <input placeholder="Password" type="password" value={password} onChange={e => setPassword(e.target.value)} className="border-2 border-pink-800 ml-2" />
-      <button onClick={register} className='bg-gray-300 p-1 ml-2'>Register</button>
-      <button onClick={login} className='bg-gray-300 p-1 ml-2'>Login</button>
 
-      {profile && 
-      <div className="mt-5">
-        <h2>Profile</h2>
-        <p>{JSON.stringify(profile, null, 2)}</p>
-      </div>}
+      {profile &&
+        <div className="mt-5">
+          <h2>Profile</h2>
+          <p>{JSON.stringify(profile, null, 2)}</p>
+        </div>}
     </div>
   );
 }
