71be69fd0390a9de94298cfe42e09f3747cdaf52
vvhh3 <vvhh3144@gmail.com>
2026-04-07 13:35:07 +0400

    CRUD

--- vite-project/src/Components/TansTackQuery/TansTackQuery.jsx
+++ vite-project/src/Components/TansTackQuery/TansTackQuery.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
 import Title from '../Home/Title';
 import NavButton from '../Home/NavButton';
 import { House } from 'lucide-react'
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
+import TanStackCRUD from './TestCRUD';
 
 const fetchData = async () => {
     const res = await axios.get(
-        "https://jsonplaceholder.typicode.com/todos?_limit=10"
+        "https://jsonplaceholder.typicode.com/todos?_limit=5"
     );
 
     return res.data; // ВСЕГДА возвращаем данные
@@ -15,6 +15,18 @@ const fetchData = async () => {
 
 
 const TansTackQuery = () => {
+
+    //const { data, isLoading } = useQuery({
+    //   queryKey: ["todos", userId], //уникальный ключ запроса
+    //   queryFn: fetchTodos, //функция, которая делает запрос
+    //   enabled: !!userId, // пока userId пустой → запрос НЕ идёт
+    //   staleTime: 1000 * 60, // сколько данные считаются свежими
+    //   cacheTime: 1000 * 60 * 5, // сколько хранить кеш после ухода компонента
+    //   retry: 2, // сколько раз повторять при ошибке
+    //   refetchOnWindowFocus: true, //обновляет данные при возврате на вкладку
+    //   select: (data) => data.slice(0, 5), //изменяет данные перед тем как отдать в компонент
+    //   refetchInterval: 5000 //авто-обновление каждые 5 сек
+    // })
 
     const {
         data,
@@ -26,39 +38,46 @@ const TansTackQuery = () => {
     } = useQuery({
         queryKey: ["todos"],
         queryFn: fetchData,
-        staleTime: 5000, // 5 сек не перезапрашивает
+        staleTime: 5000,
     });
 
-    if (isLoading) return <p>⏳ Загрузка...</p>;
-
-    if (isError) return <p>❌ {error.message}</p>;
+    
+    if (isError) return <p>❌ {error.message}</p>
     return (
         <div>
             <Title name={"TansTack Query"} />
             <NavButton title={'Home'} path={'/'} icon={<House />} />
 
-            <div className="p-4">
+            {isLoading ? <>
+                <p>⏳ Загрузка...</p>
+            </> : <>
 
-                <button
-                    onClick={() => refetch()}
-                    className=""
-                >
-                    🔄 Обновить
-                </button>
-                {isFetching && <p className="text-sm">🔄 Обновляем в фоне...</p>}
+                <div className="p-4">
 
-                <ul className="space-y-2">
-                    {data.map((todo) => (
-                        <li
-                            key={todo.id}
-                            className="p-2 border rounded-xl flex justify-between"
-                        >
-                            <span>{todo.title}</span>
-                            <span>{todo.completed ? "✅" : "❌"}</span>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+                    <button
+                        onClick={() => refetch()}
+                        className="bg-black text-white p-3 rounded-2xl w-full mb-5 
+                    dark:bg-gray-800 dark:border dark:border-gray-300 dark:text-white hover:scale-101 transition"
+                    >
+                        🔄 Обновить
+                    </button>
+                    {isFetching && <p className="text-sm">🔄 Обновляем в фоне...</p>}
+
+                    <ul className="space-y-2">
+                        {data.map((todo) => (
+                            <li
+                                key={todo.id}
+                                className="bg-white dark:bg-gray-800 p-4 rounded-2xl shadow-md"
+                            >
+                                <span className='text-2xl font-bold'>{todo.title}</span>
+                                <span>{todo.completed ? "✅" : "❌"}</span>
+                            </li>
+                        ))}
+                    </ul>
+
+                </div>
+            </>}
+            <TanStackCRUD/>
         </div>
     );
 }
