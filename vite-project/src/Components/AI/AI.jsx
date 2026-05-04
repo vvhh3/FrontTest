3d593a44555f5345e3d3948a87444bdfee7955d8
vvhh3 <vvhh3144@gmail.com>
2026-05-04 21:59:14 +0400

    simple ai chat

--- vite-project/src/Components/AI/AI.jsx
+++ vite-project/src/Components/AI/AI.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import NavButton from '../Home/NavButton'
 import Title from '../Home/Title'
 import { House } from 'lucide-react'
-export const AI = () => {
+import axios from 'axios'
+
+
+const AI = () => {
+    const [input, setInput] = useState('')
+    const [response, setResponse] = useState('')
+
+    const sendMessage = async () => {
+        try {
+            console.log("input", input)
+            const res = await axios.post("http://localhost:5000/api/chat", {
+                message: input
+            })
+            console.log("res", res)
+
+            const data = res.data
+// как сделать контекст чата , если я использую api openrouter , backend: nodeJs
+            setResponse(data.answer)
+            console.log(data.answer)
+        } catch (e) {
+            console.log(e)
+        }
+    }
     return (
         <div>
             <Title name={"AI"} />
             <NavButton title={'Home'} path={'/'} icon={<House />} />
+            <div className='flex flex-col justify-center'>
+
+                <input value={input} onChange={(e) => setInput(e.target.value)} className='border border-black w-1/2' />
+                <button onClick={sendMessage} className='bg-gray-400 hover:bg-gray-600 duration-400 rounded-2xl'>Send</button>
+
+                <p>Ответ: {response}</p>
+            </div>
         </div>
     )
 }
+export default AI
--- vite-project/src/Components/AI/server.js
+++ vite-project/src/Components/AI/server.js
@@ -1,7 +1,41 @@
 import express from 'express';
 import cors from 'cors';
+import OpenAI from "openai";
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express();
 app.use(cors())
 app.use(express.json())
 
+const client = new OpenAI({
+    baseURL: "https://openrouter.ai/api/v1",
+    apiKey: process.env.OPENROUTER_API_KEY,
+});
+
+app.post("/api/chat", async (req, res) => {
+    const { message } = req.body;
+
+    try {
+        const response = await client.chat.completions.create({
+            model: "poolside/laguna-xs.2:free",
+            messages: [
+                {
+                    role: "user",
+                    content: message,
+                },
+            ],
+        });
+
+        res.json({
+            answer: response.choices[0].message.content,
+        });
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+app.listen(5000, () => {
+    console.log("server started port: 5000")
+})
