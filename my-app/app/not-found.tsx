83cd67752f13736465e0b6d153e023c9c88de977
Матвей <vvhh3144@gmail.com>
2026-07-06 16:09:44 +0400

    Переименовал favicon.png в icon.png, обновил ссылки в layout и добавил кастомную страницу 404

---
+++ my-app/app/not-found.tsx
@@ -0,0 +1,11 @@
+
+const notFound = () => {
+
+    return(
+        <div className="flex justify-end items-start min-h-screen md:justify-center md:items-center">
+            <h1 className="text-black text-2xl">Page not found</h1>
+        </div>
+    )
+}
+
+export default notFound;
